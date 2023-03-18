d363df815732f6d55244f82b30a4cccd5d5e6024
Crann Moroney <crann@icrann.com>
2023-03-18 15:44:19 +0000

    Added Files

---
+++ Rolo/Rolo/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Rolo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Initialise";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Items.AddRange(new object[] {
+            "999",
+            "30",
+            "60",
+            "75",
+            "120",
+            "144",
+            "165",
+            "240",
+            "360"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "999";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 261);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Rolo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Rolo/Rolo/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Rolo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the path to the latest version of Roblox
+            string robloxPath = GetLatestRobloxPath();
+
+            // Create a new folder called "ClientSettings" in the Roblox directory
+            string clientSettingsPath = Path.Combine(robloxPath, "ClientSettings");
+            Directory.CreateDirectory(clientSettingsPath);
+
+            // Create a new ClientAppSettings object and set its DFIntTaskSchedulerTargetFps property
+            ClientAppSettings clientSettings = new ClientAppSettings();
+            clientSettings.DFIntTaskSchedulerTargetFps = int.Parse(comboBox1.SelectedItem.ToString());
+
+            // Serialize the ClientAppSettings object to JSON and write it to a file
+            string json = JsonConvert.SerializeObject(clientSettings, Formatting.Indented);
+            File.WriteAllText(Path.Combine(clientSettingsPath, "ClientAppSettings.json"), json);
+
+            MessageBox.Show("Roblox unlocked to selected FPS.");
+        }
+
+        private string GetLatestRobloxPath()
+        {
+            string robloxVersionsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Roblox\\Versions");
+
+            // Get the latest directory in the Roblox versions folder
+            DirectoryInfo latestDirectory = new DirectoryInfo(robloxVersionsPath).GetDirectories().OrderByDescending(d => d.CreationTime).FirstOrDefault();
+
+            if (latestDirectory != null)
+            {
+                return latestDirectory.FullName;
+            }
+            else
+            {
+                MessageBox.Show("Unable to locate Roblox installation directory.");
+                throw new Exception("Unable to locate Roblox installation directory.");
+            }
+        }
+    }
+
+    public class ClientAppSettings
+    {
+        public int DFIntTaskSchedulerTargetFps { get; set; }
+    }
+}
